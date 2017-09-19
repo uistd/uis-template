1cbe416bd62ea61ed5fa19cda0ddabc5e2f2f0ce
huangshunzhao <huangshunzhao@wanda.cn>
2017-09-19 17:32:04 +0800

    一部分初始化代码

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    //默认的curl超时时间
+    'default_curl_timeout' => 3000,
+    //环境 dev, sit, uat, prod
+    'env' => 'dev',
+    //日志目录，默认在runtime目录的logs
+    'log_path' => 'logs'
+);
