6fbe28b7c7b0d62ec9544d021d8673c8a4ca9fee
huangshunzhao <huangshunzhao@wanda.cn>
2017-09-21 23:26:55 +0800

    init tool

---
+++ apps/demo/page/ToolPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Uis\Demo\Tool;
+
+use FFan\Dop\Uis\Page;
+
+class ToolPage extends Page
+{
+    /**
+     * index
+     */
+    public function actionIndex()
+    {
+        
+    }
+}
