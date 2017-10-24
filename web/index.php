470256d815be15aae9b103adee16b2ff9ec493c1
huangshunzhao <huangshunzhao@wanda.cn>
2017-10-24 19:58:31 +0800

    增加lock和打印时间

--- web/index.php
+++ web/index.php
@@ -5,3 +5,4 @@ define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 $config = require(ROOT_PATH . 'config/config.php');
 require(ROOT_PATH . 'vendor/autoload.php');
 (new \FFan\Dop\Uis\Application($config))->run();
+debugTotalTime();
